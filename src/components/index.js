8931128680da31069e1498be68e5f6bcd996d5b8
Godsent Agundu <agundugodsent@gmail.com>
2022-08-18 01:09:42 +0100

    Initial commit for cryptocurrency app

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from './Navbar';
+export { default as Hompepage } from './Hompepage';
+export { default as Cryptocurrencies } from './Cryptocurrencies';
+export { default as CryptoDetails } from './CryptoDetails';
+export { default as News } from './News';
